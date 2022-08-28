c166ebcbb4e6a2bf29933a59ed53d477b1f0eec9
Riccardo Tesselli <riccardo.tesselli@gmail.com>
2022-08-28 23:26:17 +0200

    Simplify environment

--- server/model/environment.go
+++ server/model/environment.go
@@ -1,22 +1,20 @@
 package model
 
 import (
-	"fmt"
-
 	"github.com/rtesselli/particles/server/common"
 )
 
 type Environment struct {
-	particles     []*LivingParticle
-	width, height int
+	output_positions *common.SafeMap[int, Particle]
+	width, height    int
 }
 
-func NewEnvironment(width, height int) Environment {
-	return Environment{width: width, height: height}
+func NewEnvironment(width, height int, output_positions *common.SafeMap[int, Particle]) Environment {
+	return Environment{width: width, height: height, output_positions: output_positions}
 }
 
 func (e *Environment) AddParticle(particle Particle) {
-	e.particles = append(e.particles, &LivingParticle{particle})
+	go LivingParticle{particle}.Live(e.output_positions)
 }
 
 func (e *Environment) Width() int {
@@ -25,13 +23,4 @@ func (e *Environment) Width() int {
 
 func (e *Environment) Height() int {
 	return e.height
-}
-
-func (e *Environment) Start(output_positions *common.SafeMap[int, Particle]) {
-	fmt.Println("Starting environment")
-	for idx, particle := range e.particles {
-		fmt.Printf("Starting particle with id: %d\n", idx)
-		go particle.Live(output_positions)
-	}
-	fmt.Println("Done")
 }
--- server/model/environment_test.go
+++ server/model/environment_test.go
@@ -8,12 +8,11 @@ import (
 )
 
 func TestStart(t *testing.T) {
-	environment := NewEnvironment(10, 10)
+	positions := common.NewSafeMap[int, Particle]()
+	environment := NewEnvironment(10, 10, &positions)
 	environment.AddParticle(NewStaticParticle(10, 10, 1, 1, 10))
 	environment.AddParticle(NewStaticParticle(10, 10, 2, 1, 20))
 	environment.AddParticle(NewStaticParticle(10, 10, 3, 1, 30))
-	positions := common.NewSafeMap[int, Particle]()
-	environment.Start(&positions)
 	time.Sleep(2 * time.Second) // TODO find better way to wait for goros
 	if len(positions.GetMap()) != 3 {
 		t.Errorf("Wrong size")
