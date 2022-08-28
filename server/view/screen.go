9a8943cff7ad34f73c28c2e598e3da5a43c771ea
Riccardo Tesselli <riccardo.tesselli@gmail.com>
2022-08-28 23:57:29 +0200

    Show TPS

--- server/view/screen.go
+++ server/view/screen.go
@@ -1,20 +1,36 @@
 package view
 
 import (
+	"fmt"
+	"image/color"
+
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/rtesselli/particles/server/common"
 	"github.com/rtesselli/particles/server/model"
+
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/opentype"
+
+	"github.com/hajimehoshi/ebiten/v2/examples/resources/fonts"
+	"github.com/hajimehoshi/ebiten/v2/text"
 )
 
 type View struct {
 	positions     *common.SafeMap[int, model.Particle]
 	width, height int
+	font          font.Face
 }
 
 func NewView(width, height int, positions *common.SafeMap[int, model.Particle]) *View {
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("Particles")
-	return &View{width: width, height: height, positions: positions}
+	tt, _ := opentype.Parse(fonts.MPlus1pRegular_ttf)
+	font, _ := opentype.NewFace(tt, &opentype.FaceOptions{
+		Size:    10,
+		DPI:     72,
+		Hinting: font.HintingFull,
+	})
+	return &View{width: width, height: height, positions: positions, font: font}
 }
 
 func (v *View) Update() error {
@@ -22,7 +38,8 @@ func (v *View) Update() error {
 }
 
 func (v *View) Draw(screen *ebiten.Image) {
-
+	msg := fmt.Sprintf("TPS: %0.2f", ebiten.CurrentTPS())
+	text.Draw(screen, msg, v.font, 20, 40, color.White)
 }
 
 func (v *View) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
