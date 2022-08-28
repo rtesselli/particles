cbb8cc597139e7fd930beb9df82aa7726507bb97
Riccardo Tesselli <riccardo.tesselli@gmail.com>
2022-08-29 00:37:14 +0200

    Draw a red circle

--- server/view/screen.go
+++ server/view/screen.go
@@ -2,6 +2,7 @@ package view
 
 import (
 	"fmt"
+	"image"
 	"image/color"
 
 	"github.com/hajimehoshi/ebiten/v2"
@@ -13,6 +14,8 @@ import (
 
 	"github.com/hajimehoshi/ebiten/v2/examples/resources/fonts"
 	"github.com/hajimehoshi/ebiten/v2/text"
+
+	"github.com/fogleman/gg"
 )
 
 type View struct {
@@ -40,6 +43,9 @@ func (v *View) Update() error {
 func (v *View) Draw(screen *ebiten.Image) {
 	msg := fmt.Sprintf("TPS: %0.2f", ebiten.CurrentTPS())
 	text.Draw(screen, msg, v.font, 20, 40, color.White)
+	img := ebiten.NewImageFromImage(Circle())
+	op := &ebiten.DrawImageOptions{}
+	screen.DrawImage(img, op)
 }
 
 func (v *View) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -50,4 +56,12 @@ func (v *View) Run() {
 	if err := ebiten.RunGame(v); err != nil {
 		panic(err)
 	}
+}
+
+func Circle() image.Image {
+	dc := gg.NewContext(100, 100)
+	dc.DrawCircle(50, 50, 25)
+	dc.SetRGB(1, 0, 0)
+	dc.Fill()
+	return dc.Image()
 }
